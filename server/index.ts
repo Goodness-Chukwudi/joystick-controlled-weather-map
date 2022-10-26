f616f20d2506a70c4c2f2ec263609f86e793a430
Goodness-Chukwudi <chukwudi.ibeche@moninetworks.com>
2022-10-26 19:35:14 +0100

    adjusted port number

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ const app = express()
 import path from "path";
 
 app.use("/", express.static(path.resolve(__dirname, "../client")))
-const port = 5000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`);
 });
