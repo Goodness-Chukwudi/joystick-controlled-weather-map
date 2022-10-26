2122c47b2e851c233f9ab1c1e580259ba5eef36e
Goodness-Chukwudi <chukwudi.ibeche@moninetworks.com>
2022-10-26 20:01:55 +0100

    switched to wss for deployment

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 
-const url = "ws://weather-man-map.herokuapp.com/";
+const url = "wss://weather-man-map.herokuapp.com";
 const socket = new WebSocket(url);
 const gif = document.getElementById("gif");
 const x = gif.offsetTop;
